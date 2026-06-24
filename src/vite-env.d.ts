c09738a40412172c6a0519d4124869cd482374cc
jteshaboev1984-ops <j.teshaboev1984@gmail.com>
2026-06-24 20:46:55 +0500

    Type remote story state endpoint

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_QISSA_STORY_PROVIDER?: 'local' | 'remote'
   readonly VITE_QISSA_STORY_ENDPOINT?: string
+  readonly VITE_QISSA_STATE_ENDPOINT?: string
   readonly VITE_QISSA_SUPABASE_PUBLISHABLE_KEY?: string
   readonly VITE_QISSA_STORY_TIMEOUT_MS?: string
   readonly VITE_QISSA_STORY_FALLBACK_TO_LOCAL?: 'true' | 'false'
