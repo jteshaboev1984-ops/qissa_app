e7e145d1466bf6d229ab19fcbe21a6d629cd942b
jteshaboev1984-ops <j.teshaboev1984@gmail.com>
2026-06-26 17:29:02 +0500

    Guard story copy override lookup

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -3,6 +3,6 @@ import { storyQualityCopy } from '../i18n/storyQualityCopy'
 import type { Language } from '../types/qissa'
 
 export const t = (language: Language, key: I18nKey): string => {
-  const override = storyQualityCopy[language][key]
+  const override = storyQualityCopy[language]?.[key]
   return override ?? dictionaries[language][key]
 }
