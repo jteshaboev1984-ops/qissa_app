5aaee131dbc886f921752f66eb4a67ec142569d3
jteshaboev1984-ops <j.teshaboev1984@gmail.com>
2026-06-26 17:11:52 +0500

    Use story quality translations

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,4 +1,8 @@
 import { dictionaries, type I18nKey } from '../i18n/dictionaries'
+import { storyQualityCopy } from '../i18n/storyQualityCopy'
 import type { Language } from '../types/qissa'
 
-export const t = (language: Language, key: I18nKey): string => dictionaries[language][key]
+export const t = (language: Language, key: I18nKey): string => {
+  const override = storyQualityCopy[language][key]
+  return override ?? dictionaries[language][key]
+}
