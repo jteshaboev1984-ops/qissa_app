9186da3459b757075c265d81a619bf6a83381466
jteshaboev1984-ops <j.teshaboev1984@gmail.com>
2026-09-25 15:05:00 +0500

    release: add sevenRoadsUiAssets.ts

---
+++ src/data/sevenRoadsUiAssets.ts
@@ -0,0 +1,9 @@
+const SUPABASE_PUBLIC_ROOT =
+  'https://phwakdpxxyncyslvnqht.supabase.co/storage/v1/object/public/story-images/seven-roads/ui_v1'
+
+export const sevenRoadsUiAssets = {
+  welcome: `${SUPABASE_PUBLIC_ROOT}/seven_roads_welcome_world_v1.webp?v=20260925-100148`,
+  home: `${SUPABASE_PUBLIC_ROOT}/seven_roads_home_approach_v1.webp?v=20260925-100148`,
+  library: `${SUPABASE_PUBLIC_ROOT}/seven_roads_library_hall_v1.webp?v=20260925-100148`,
+  futureSeasonPlaceholder: `${SUPABASE_PUBLIC_ROOT}/seven_roads_future_season_placeholder_v1.webp?v=20260925-100148`,
+} as const
