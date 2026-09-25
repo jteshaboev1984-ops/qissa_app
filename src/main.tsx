0f38194ee2792c644c6d7599593111e2cf409fd3
jteshaboev1984-ops <j.teshaboev1984@gmail.com>
2026-09-25 10:47:14 +0500

    refactor: remove legacy AI bootstrap from Seven Roads startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,38 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { migratePersistedStoryIntoClosedBetaScope } from './lib/closedBetaMigration'
-import { localPersistence } from './lib/localPersistence'
-import { storyStateService } from './lib/storyStateService'
 import './index.css'
 
-const renderApp = () => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  )
-}
-
-const bootstrap = async () => {
-  try {
-    await localPersistence.waitForPendingRemoteReset()
-    await localPersistence.waitForPendingChoiceSync()
-    const snapshot = await storyStateService.loadCurrent()
-
-    if (snapshot) {
-      localPersistence.restoreRemoteSnapshot(snapshot)
-    }
-  } catch (error) {
-    console.error('Failed to restore remote story state', error)
-  } finally {
-    try {
-      migratePersistedStoryIntoClosedBetaScope()
-    } catch (error) {
-      console.error('Failed to migrate persisted story into the closed-beta scope', error)
-    }
-    renderApp()
-  }
-}
-
-void bootstrap()
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
